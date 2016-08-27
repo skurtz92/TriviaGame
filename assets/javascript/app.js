cf7c91601342373923cfc7249cd4335e5388d417
skurtz92 <sydneykurtz@Sydneys-MacBook-Air.local>
2016-08-27 18:27:47 -0500

    updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,147 @@
+
+var panel = $('#quiz-area');
+var countStartNumber = 30;
+
+
+$(document).on('click', '#start-over', function(e) {
+  game.reset();
+});
+
+$(document).on('click', '.answer-button', function(e) {
+  game.clicked(e);
+});
+
+$(document).on('click', '#start', function(e) {
+  $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
+  game.loadQuestion(); // <span> allows you to style CSS or manipulate it with javascript
+});
+
+
+var questions = [{
+  question: "How many times have the Cowboys won the superbowl?",
+  answers: ["5", "10", "7", "2"],
+  correctAnswer: 5,
+}, {
+  question: "What year did the Dallas Cowboys join the NFL?",
+  answers: ["1982", "1950", "1960", "1971"],
+  correctAnswer: 1960,
+
+}, {
+  question: "This Cowboys receiver was drafted from Oklahoma State in the 2010 NFL Draft and wears number 88?",
+  answers: ["Dez Bryant", "Tony Romo", "Jason Witten", "Demarcus Lawrence"],
+  correctAnswer: "Dez Bryant",
+
+}, {
+  question: 'Which team ties with the Dallas Cowboys in Super Bowl wins?',
+  answers: ["49ers", "Steelers", "Patriots", "Seahawks"],
+  correctAnswer: "49ers",
+
+}, {
+  question: 'Who did Troy Aikman win Super Bowl XXVII MVP against?',
+  answers: ["Steelers", "Buffalo Bills", "Patriots", "Broncos"],
+  correctAnswer: "Buffalo Bills",
+
+}, {
+  question: 'Who defeated Dallas in the Ice Bowl?',
+  answers: ["Saints", "Buffalo Bills", "Green Bay", "Browns"],
+  correctAnswer: "Green Bay",
+  
+  }];
+
+
+
+
+var game = {
+  questions:questions,
+  currentQuestion:0,
+  counter:countStartNumber,
+  correct:0,
+  incorrect:0,
+  countdown: function(){
+    game.counter--;
+    $('#counter-number').html(game.counter);
+
+    if (game.counter === 0){
+      console.log('TIMEZ UP');
+      game.timeUp();
+    }
+  },
+  loadQuestion: function(){
+    timer = setInterval(game.countdown, 1000);
+    panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+    for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+      panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+    }
+  },
+  nextQuestion: function(){
+    game.counter = countStartNumber;
+    $('#counter-number').html(game.counter);
+    game.currentQuestion++;
+    game.loadQuestion();
+  },
+  timeUp: function (){
+    clearInterval(timer);
+    $('#counter-number').html(game.counter);
+
+    panel.html('<h2>Out of Time!</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+    panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  results: function() {
+    clearInterval(timer);
+
+    panel.html('<h2>All done, heres how you #scored</h2>');
+    $('#counter-number').html(game.counter);
+    panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+    panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+    panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+    panel.append('<br><button id="start-over">Start Over?</button>');
+  },
+  clicked: function(e) {
+    clearInterval(timer);
+
+    if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+      this.answeredCorrectly();
+    } else {
+      this.answeredIncorrectly();
+    }
+  },
+  answeredIncorrectly: function() {
+    game.incorrect++;
+    clearInterval(timer);
+    panel.html('<h2>Nope!</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+    
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  answeredCorrectly: function(){
+    clearInterval(timer);
+    game.correct++;
+    panel.html('<h2>Correct!</h2>');
+    
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  reset: function(){
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
+};
